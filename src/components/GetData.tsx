ac8ba9bb49c5a8ee86e53164146be44415254bbd
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2023-03-23 14:18:48 -0400

    centered tabs

--- src/components/GetData.tsx
+++ src/components/GetData.tsx
@@ -7,7 +7,6 @@ const RANGE = "A2:D6";
 export default function GetData() {
   const [data, setData] = useState<any>();
 
-
   useEffect(() => {
     fetch(
       `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${process.env.REACT_APP_API_KEY}`
@@ -15,66 +14,60 @@ export default function GetData() {
       .then((response) => response.json())
       .then((result) => {
         const todayData = result.values.filter((val: any) => {
-          
           const todayDateString = new Date().toLocaleDateString(); // convert today's date to a string in the format 'mm/dd/yyyy'
 
           const timestampDate = new Date(val[0]);
           const timestampDateString = timestampDate.toLocaleDateString();
 
-          return todayDateString === timestampDateString
-        })
+          return todayDateString === timestampDateString;
+        });
 
         const jsonTodayData = todayData.map((item: any) => ({
-          "date": item[0],
-          "score": parseInt(item[1]),
-          "major": item[2],
-          "year": item[3]
-        }))
+          date: item[0],
+          score: parseInt(item[1]),
+          major: item[2],
+          year: item[3],
+        }));
 
-        setData(jsonTodayData)
-
-      
-      
+        setData(jsonTodayData);
       })
       .catch((error) => console.log(error));
-
   }, []);
 
-
   const getAverageScore = () => {
-    return data?.reduce((acc: number, curr: any) => curr.score + acc, 0) / data?.length
-  }
+    return (
+      data?.reduce((acc: number, curr: any) => curr.score + acc, 0) /
+      data?.length
+    );
+  };
 
   const getFrequencies = () => {
-
-    const scoreCounter: any = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
-    const majorCounter: any = {}
-    const yearCounter: any = {}
+    const scoreCounter: any = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+    const majorCounter: any = {};
+    const yearCounter: any = {};
 
     data?.forEach((val: any) => {
-
-      const {score, major, year} = val
-
+      const { score, major, year } = val;
 
       scoreCounter[score] = (scoreCounter[score] || 0) + 1;
       majorCounter[major] = (majorCounter[major] || 0) + 1;
       yearCounter[year] = (yearCounter[year] || 0) + 1;
+    });
 
-    })
+    return { scoreCounter, majorCounter, yearCounter };
+  };
 
-    return {scoreCounter, majorCounter, yearCounter}
-  }
-
-  const {scoreCounter, majorCounter, yearCounter} = getFrequencies()
+  const { scoreCounter, majorCounter, yearCounter } = getFrequencies();
 
   return (
     <div>
+      <h3>Data for {new Date().toLocaleDateString()} </h3>
+      <h4>Responses: {data?.length}</h4>
+      <h4>Average Score: {getAverageScore().toFixed(1)}</h4>
 
-      <h3>Average Score for today: {getAverageScore().toFixed(1)}</h3>
-
-      <BarChart data={scoreCounter} chartName="Scores:"/>
-      <BarChart data={majorCounter} chartName="Majors:"/>
-      <BarChart data={yearCounter} chartName="Years:"/>
+      <BarChart data={scoreCounter} chartName="Scores:" />
+      <BarChart data={majorCounter} chartName="Majors:" />
+      <BarChart data={yearCounter} chartName="Years:" />
     </div>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,6 @@ import { Box, Tab, Tabs, Typography } from "@mui/material";
 import { useState } from "react";
 import GetData from "./GetData";
 
-
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
@@ -30,30 +29,38 @@ function TabPanel(props: TabPanelProps) {
 }
 
 export default function Home() {
-
   const [value, setValue] = useState(0);
-  
 
-  const form = <iframe title="form" src="https://docs.google.com/forms/d/e/1FAIpQLSeFuJS3RXD94spL-5nlXSjbtfGquamZ75sdVz69SJCRgp340A/viewform?embedded=true" width="100%" height="772" >Loading…</iframe>
-
+  const form = (
+    <iframe
+      title="form"
+      src="https://docs.google.com/forms/d/e/1FAIpQLSeFuJS3RXD94spL-5nlXSjbtfGquamZ75sdVz69SJCRgp340A/viewform?embedded=true"
+      width="100%"
+      height="772"
+    >
+      Loading…
+    </iframe>
+  );
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
-  return <Box sx={{ width: '100%' }}>
-  <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-      <Tab label="Form"/>
-      <Tab label="Results!" />
-    </Tabs>
-  </Box>
-  <TabPanel value={value} index={0}>
-    {form}
-    <h1>See results page!</h1>
-  </TabPanel>
-  <TabPanel value={value} index={1}>
-    <GetData/>
-  </TabPanel>
-</Box>
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={value} onChange={handleChange} centered>
+          <Tab label="Form" />
+          <Tab label="Results!" />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <h2>See results page! ^^</h2>
+        {form}
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <GetData />
+      </TabPanel>
+    </Box>
+  );
 }
